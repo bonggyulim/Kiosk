948edc1b120a3ad9e448d6a3f41b6c580592411e
bonggyulim <qhdrbdhkdwks@gmail.com>
2024-03-14 15:34:15 +0900

    장바구니 목록, 총 금액 출력 클래스

--- menuchoice/classdirectory/ShoppingBasket.kt
+++ menuchoice/classdirectory/ShoppingBasket.kt
@@ -3,8 +3,8 @@ package com.example.kiosk.menuchoice.classdirectory
 import com.example.kiosk.main.menuList
 
 class ShoppingBasket() {
-    companion object {
-        fun list(menuList: List<Menu.TopMenu>): String {
+    companion object {  // 싱글톤을 사용하여 어디서든 접근 가능
+        fun list(menuList: List<Menu.TopMenu>): String {    // 지금까지 담은 메뉴를 출력하는 함수
             var str: String = ""
             var menuList = menuList
             for (i in 0..7) {
@@ -15,7 +15,7 @@ class ShoppingBasket() {
             return str.toString()
         }
 
-        fun price(): Int {
+        fun price(): Int {  // 장바구니에 담은 메뉴의 총 가격을 출력하는 함수
             var sum: Int = 0
             for (i in 0..7) {
                 sum = sum + menuList.get(i).price * menuList.get(i).count
