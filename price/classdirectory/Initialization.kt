b6a669b6f6a5afe65d5d6d006675a42163a3fc52
bonggyulim <qhdrbdhkdwks@gmail.com>
2024-03-14 15:36:32 +0900

    장바구니초기화클래스

--- price/classdirectory/Initialization.kt
+++ price/classdirectory/Initialization.kt
@@ -2,7 +2,7 @@ package com.example.kiosk.price.classdirectory
 
 import com.example.kiosk.menuchoice.classdirectory.Menu
 
-class Initialization {
+class Initialization {  // 장부구니를 초기화하는 클래스, 그냥 함수로 만들어도 될것같다
     companion object {
         fun initialization(menuList: List<Menu.TopMenu>) {
             for(i in 0..7){
